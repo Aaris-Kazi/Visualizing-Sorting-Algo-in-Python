e2e0e74e0a718f5c3d726111aef245b53552b5d9
Aaris Kazi <40735215+Aaris-Kazi@users.noreply.github.com>
2020-09-19 22:45:52 +0530

    🌟 This is the first file with a minor issue but it will work for you

---
+++ Bubble_sort.py
@@ -0,0 +1,53 @@
+import cv2 as c
+import numpy as np
+import time
+
+def sorty(a, p):
+    img = np.zeros((512,512,4),np.uint8)
+    n = len(a)
+    for i in range(n):
+        swap = False
+        for j in range(0, n-1-i):
+            
+            if a[j] > a[j+1] :
+                
+                
+                c.line(img, (20,155+p[j]), (200+a[j], 155+p[j]), (0,0 ,0),5)
+                t = a[j]
+                a[j] = a[j+1]
+                a[j+1] = t
+                
+                c.line(img, (20,155+p[j]), (200+a[j], 155+p[j]), (255,0 ,255),5)
+                c.line(img, (20,155+p[j+1]), (200+a[j+1], 155+p[j+1]), (0,0 ,0),5)
+                c.line(img, (20,155+p[j+1]), (200+a[j+1], 155+p[j+1]), (255,0 ,255),5)
+                
+                c.imshow('Animation Window', img)
+                k = c.waitKey(1000)
+                if k == 270:
+                    break
+                
+                swap = True
+        if swap == False:
+            break
+        
+    c.destroyAllWindows()
+
+def ask():
+    a = []
+    p = []
+    t= 5
+    c = int(input('How many lines do you want:-'))
+    for i in range(0, c) :
+        x = int(input('Enter the Value:- '))
+        a.append(x)
+        p.append(t)
+        t += 20
+    return a, p
+
+def main():
+    a, p = ask()
+    sorty(a, p)
+
+if __name__ == "__main__":
+    main()
+    ## Copyrights Reserves to Aaris Kazi for the Visualising the Algorithm
